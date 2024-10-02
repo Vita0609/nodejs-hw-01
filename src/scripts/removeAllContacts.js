dd0dfa045786246604178ee3b63822471acd2f9c
Vita <nikaangel295@gmail.com>
2024-10-02 06:48:06 +0100

    add

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -2,7 +2,13 @@ import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
 export const removeAllContacts = async () => {
-  await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+    console.log('All contacts successfully removed.');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+    throw error;
+  }
 };
 
 removeAllContacts();
